25c516f4029c5936ffe2e77bf21a63309aaf3cf1
Aryan <aryan.rajalbandi@gmail.com>
2022-03-02 15:54:25 +0530

    changed

--- MQ/main/urls.py
+++ MQ/main/urls.py
@@ -2,7 +2,10 @@ from cgi import test
 from django.urls import path
 from .views import *
 
+create_paper = CreatePaper()
+
 urlpatterns = [
     path("", home, name="home"),
-    path("test/", test1, name="test")
+    path("test/", test1, name="test"),
+    path("create/", create_paper.create_paper_1, name="view_paper")
 ]
--- MQ/main/views.py
+++ MQ/main/views.py
@@ -7,6 +7,17 @@ def home(request):
     return render(request, "index.html")
 
 
+class CreatePaper:
+    def init(self):
+        pass
+
+    def create_paper_1(self, request):
+        if request.method == "GET":
+            return render(request, "create_paper/create_paper_1.html")
+        else:
+            print("in the POST method")
+
+
 def test1(request):
     return render(request, "base.html")
 
